68914e54b23bd727e344a7824f2ba9cdb58e8f76
Pomiraiu <101802475+Pomiraiu@users.noreply.github.com>
2023-07-01 00:39:49 +0300

    Update index.js

--- projects/functions/index.js
+++ projects/functions/index.js
@@ -12,7 +12,10 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {}
+function returnFirstArgument(first) {
+ return first;
+}
+
 
 /*
  Задание 2:
@@ -28,7 +31,9 @@ function returnFirstArgument() {}
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults() {}
+function sumWithDefaults(a, b = 100) {
+ return a + b;
+}
 
 /*
  Задание 3:
@@ -38,7 +43,9 @@ function sumWithDefaults() {}
  Пример:
    returnFnResult(() => 'привет') вернет 'привет'
  */
-function returnFnResult() {}
+function returnFnResult(fn) {
+ return fn();
+}
 
 /*
  Задание 4:
@@ -53,7 +60,14 @@ function returnFnResult() {}
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter() {}
+function returnCounter(n = 0) {
+ function (F){
+  n+=1;
+  return n;
+}
+ return F;
+}
+
 
 /*
  Задание 5 *:
@@ -64,7 +78,9 @@ function returnCounter() {}
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {}
+function returnArgumentsArray(...args) {
+ return args;
+}
 
 export {
   returnFirstArgument,
